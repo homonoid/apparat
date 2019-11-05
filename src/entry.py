7495d8cf3a1f39e45a618ec1f5f262e472a0b93e
Alexey Yurchenko <homonoidian@yandex.ru>
2019-11-05 21:12:17 +0300

    Factorial example; major entry.py rewrite; major fixes

--- src/entry.py
+++ src/entry.py
@@ -1,39 +1,67 @@
-#from lark import Lark
 from syntax.parse import Lark_StandAlone, LarkError
 from colorclass import Color
 import readline
 
-#parser = Lark.open(
-#  'syntax/apparat.lark',
-#  rel_to=__file__,
-#  debug=True,
-#  parser='lalr',
-#  start='root',
-#)
+def error(message):
+  head = Color('{autored}=== SORRY! ==={/red}')
+  print(f'\033[1;m{head}\033[0m\n{message}\n')
 
-def main():
+def parse(parser, source, filename = 'in'):
+  try:
+    return True, parser.parse(source)
+  except LarkError as err:
+    cause = source[err.pos_in_stream]
+
+    error(f'Syntax error caused by \'{cause}\' ' \
+          f'at line {err.line}, column {err.column} (of <{filename}>)')
+
+    return False, ()
+
+def main(argv):
   parser = Lark_StandAlone()
 
-  print("Apparat rev. 0. 1")
+  justParse, enableRepl = False, True
 
-  while True:
-    source = input(Color('{autogreen}~{/green} '))
+  try:
+    for arg in argv:
+      if arg in ['-ast', '-a']:
+        justParse = True
+      elif arg in ['-bytes', '-b']:
+        raise NotImplementedError('Bytecode generation not implemented yet')
+      elif arg in ['-help', '-h']:
+        enableRepl = False # so we exit right after we print
 
-    if len(source.strip()) > 0:
-      count = source.count
-  
-      while [count('{'), count('('), count('[')] != [count('}'), count(')'), count(']')]:
-        source += '\n' +  input('... ')
-        count = source.count
+        print ('Usage:\n apparat [PARAM] {FILE1 FILE2} ...\n\n' \
+               'PARAMs:\n -help, -h  -- print this message\n' \
+                        ' -ast, -a   -- verbose AST\n' \
+                        ' -bytes, -b -- verbose bytecode\n\n' \
+                'NOTE: REPL is started when there are no arguments\n')
+      else:
+        enableRepl = False
+
+        with open(arg, 'r') as source:
+          if justParse:
+            status, ast = parse(parser, source.read(), arg)
+            print(ast.pretty()) if status is not False else None
+
+    if enableRepl:
+      while True:
+        source = input(Color('{autogreen}~{/green} '))
+
+        if len(source.strip()) > 0:
+          count = source.count
       
-      try:
-        ast = parser.parse(source).pretty()
-        print(ast)
-      except LarkError as err:
-        head = Color('{autored}=== SORRY! ==={/red}')
-        cause = source[err.pos_in_stream]
-        print(f'\033[1;m{head}\033[0m\n Syntax error caused by \'{cause}\'',
-              f'at line {err.line}, column {err.column} (of <stdin>)\n')
+          while [count('{'), count('('), count('[')] != [count('}'), count(')'), count(']')]:
+            source += '\n' +  input('... ')
+            count = source.count
+          
+          if justParse:
+            status, ast = parse(parser, source)
+            print(ast.pretty()) if status is not False else None
+            
+  except FileNotFoundError as err:
+    error(f'File \'{err.filename}\' not found')
 
 if __name__ == '__main__':
-  main()
+  import sys
+  main(sys.argv[1:])
